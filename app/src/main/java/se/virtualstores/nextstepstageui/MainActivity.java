c344e87f776fc2e1b597ffd23b7b7d090c076e38
cj <carljohan.dahlman@virtualstores.se>
2019-01-03 13:30:20 +0100

    initial commit

---
+++ app/src/main/java/se/virtualstores/nextstepstageui/MainActivity.java
@@ -0,0 +1,25 @@
+package se.virtualstores.nextstepstageui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+
+
+        Picasso.with(this).load("file:///android_asset/main.png").into((ImageView) findViewById(R.id.Image_View));
+
+    }
+}
